43d2f125e7cc2b0b654321e6f67d08d86fa245e2
Adedayo Ayomide <adefeyisayo998@gmail.com>
2022-12-22 10:40:22 +0100

    pascal triangle done and dusted

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,21 @@
+def pascal_triangle(n):
+    pascal_list = [[1], [1, 1]]
+    if n == 1:
+        pascal_list = [1]
+        return (pascal_list)
+    if n == 2:
+        return pascal_list
+    for i in range(3, n+1):
+        previous = pascal_list[-1]
+        prev_len = len(previous)
+        temp = list(range(prev_len + 1))
+        for x in range(prev_len):
+            if x == 0:
+                temp[x] = previous[x]
+            else:
+                temp[x] = previous[x] + previous[x-1]
+        temp[-1] = 1
+        previous = temp
+        pascal_list.append(temp)
+    return pascal_list
+print(pascal_triangle(6))
