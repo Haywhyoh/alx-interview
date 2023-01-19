defece203c7936ba555e01960c285a6177bae1a2
Adedayo Ayomide <adefeyisayo998@gmail.com>
2023-01-19 13:45:02 +0100

    my final update

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,45 +1,41 @@
 #!/usr/bin/python3
-""" script that reads stdin line by line and computes metrics """
-if __name__ == '__main__':
-    import sys
-    total_size = 0
-    status_dict = {
-                    '200': 0,
-                    '301': 0,
-                    '400': 0,
-                    '401': 0,
-                    '403': 0,
-                    '404': 0,
-                    '405': 0,
-                    '500': 0,
-                }
-    i = 0
-    for line in sys.stdin:
+"""Input stats"""
+import sys
+
+stats = {
+    '200': 0,
+    '301': 0,
+    '400': 0,
+    '401': 0,
+    '403': 0,
+    '404': 0,
+    '405': 0,
+    '500': 0
+}
+sizes = [0]
+
+
+def print_stats():
+    print('File size: {}'.format(sum(sizes)))
+    for s_code, count in sorted(stats.items()):
+        if count:
+            print('{}: {}'.format(s_code, count))
+
+
+try:
+    for i, line in enumerate(sys.stdin, start=1):
+        matches = line.rstrip().split()
         try:
-            line_array = line.split()
-            i = i + 1
-            total_size = int(line_array[-1]) + total_size
-            status_dict[line_array[-2]] = int(status_dict[line_array[-2]]) + 1
-    
-            if i % 10 == 0:
-                print("File size: {}".format(total_size))
-                print('200: {}'.format(status_dict['200']))
-                print('301: {}'.format(status_dict['301']))
-                print('400: {}'.format(status_dict['400']))
-                print('401: {}'.format(status_dict['401']))
-                print('403: {}'.format(status_dict['403']))
-                print('404: {}'.format(status_dict['404']))
-                print('405: {}'.format(status_dict['405']))
-                print('500: {}'.format(status_dict['500']))
-        except KeyboardInterrupt:
-            """ Keyboard interruption, print from the beginning """
-            print("File size: {}".format(total_size))
-            print('200: {}'.format(status_dict['200']))
-            print('301: {}'.format(status_dict['301']))
-            print('400: {}'.format(status_dict['400']))
-            print('401: {}'.format(status_dict['401']))
-            print('403: {}'.format(status_dict['403']))
-            print('404: {}'.format(status_dict['404']))
-            print('405: {}'.format(status_dict['405']))
-            print('500: {}'.format(status_dict['500']))
-            raise
+            status_code = matches[-2]
+            file_size = matches[-1]
+            if status_code in stats.keys():
+                stats[status_code] += 1
+            sizes.append(int(file_size))
+        except Exception:
+            pass
+        if i % 10 == 0:
+            print_stats()
+    print_stats()
+except KeyboardInterrupt:
+    print_stats()
+    raise
